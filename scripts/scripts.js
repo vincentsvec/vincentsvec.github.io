a8f57ede662464febe592c5a69d465d49a58f0f9
Vincent <vindo.svec@gmail.com>
2021-10-05 21:40:51 +0200

    updates

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,12 +12,11 @@ let foreground = document.getElementById("foreground");
 
 function animateScroll() {
     let value = content.scrollTop
+    console.log(value, window.innerHeight)
 
-    sun.style.transform = "translate(0vh, " + value * 0.05 + "vh)";
-    foreground.style.transform = "translate(0vh, " + value * (-0.05) + "vh)";
-    birds.style.transform = "translate(" + value * 0.01 + "vh, " + value * (-0.01) + "vh)";
-    hills.style.transform = "translate(0vh, " + value * (-0.01) + "vh)";
-    hills_2.style.transform = "translate(0vh, " + value * (-0.005) + "vh)";
-    mountains.style.transform = "translate(0vh, " + value * 0.005 + "vh)";
-    bg.style.transform = "translate(0vh, " + value * (-0.005) + "vh)";
+    if (value <= window.innerHeight) {
+        sun.style.transform = "translate(0vh, " + value * 0.05 + "vh)";
+        foreground.style.transform = "translate(0vh, " + value * (-0.05) + "vh)";
+        birds.style.transform = "translate(" + value * 0.01 + "vh, " + value * (-0.01) + "vh)";
+    }
 }
