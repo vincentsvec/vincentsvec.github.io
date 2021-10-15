f008a22254a465c33d2c4110e9a15826571a7388
Vincent <vindo.svec@gmail.com>
2021-10-15 15:49:27 +0200

    updates

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,10 +2,6 @@ let content = document.getElementById("content")
 content.onscroll = function () { animateScroll() };
 
 let sun = document.getElementById("sun");
-let mountains = document.getElementById("mountains");
-let bg = document.getElementById("bg");
-let hills_2 = document.getElementById("hills-2");
-let hills = document.getElementById("hills");
 let birds = document.getElementById("birds");
 let foreground = document.getElementById("foreground");
 
