a375665664e31fbdc6e3d49a72f7fb4d90c0f6ed
Vinco <vindo.svec@gmail.com>
2021-10-19 21:10:38 +0200

    about me updates

--- scripts/navbar_script.js
+++ scripts/navbar_script.js
@@ -1,31 +1,23 @@
 // active underlines
-var links = document.getElementsByClassName("link-nav");
-for (var i = 0; i < links.length; i++) {
-    links[i].addEventListener("click", function () {
-        var active = document.getElementsByClassName("active");
-        active[0].className = active[0].className.replace(" active", "");
-        this.className += " active";
-    });
-}
-
 content.addEventListener("scroll", function () {
     value = content.scrollTop;
     if (value < content.clientHeight) {
-        addActive(document.querySelector(".home"));
+        addActive(".home");
     }
     else if (value >= content.clientHeight && content.clientHeight * 2 > value) {
-        addActive(document.querySelector(".about-me"));
+        addActive(".about-me");
     }
     else if (value >= content.clientHeight * 2 && content.clientHeight * 3 > value) {
-        addActive(document.querySelector(".skills"));
+        addActive(".skills");
     }
     else if (value >= content.clientHeight * 3) {
-        addActive(document.querySelector(".contact"));
+        addActive(".contact");
     }
 
 })
 
-function addActive(current) {
+function addActive(link) {
+    var current = document.querySelector(link)
     var active = document.getElementsByClassName("active");
     active[0].className = active[0].className.replace(" active", "");
     current.className += " active";
